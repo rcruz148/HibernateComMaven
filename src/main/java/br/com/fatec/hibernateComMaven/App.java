d4ec9f8afd39f2ce5f8f821f7013a27a02b8f167
rcruz148 <ronandocarmo@hotmail.com>
2015-04-05 21:17:04 -0300

    Criando projeto Hibernate com Maven

---
+++ src/main/java/br/com/fatec/hibernateComMaven/App.java
@@ -0,0 +1,13 @@
+package br.com.fatec.hibernateComMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
