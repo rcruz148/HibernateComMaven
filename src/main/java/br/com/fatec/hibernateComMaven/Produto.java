ccfd73166372111c21c71363ab6caa5de57b5c56
rcruz148 <ronandocarmo@hotmail.com>
2015-04-05 21:24:20 -0300

    Criando as classes e persistence.xml

---
+++ src/main/java/br/com/fatec/hibernateComMaven/Produto.java
@@ -0,0 +1,34 @@
+package br.com.fatec.hibernateComMaven;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Produto {
+	@Id
+	@GeneratedValue
+	int id;
+	String nome;
+	double preco;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public double getPreco() {
+		return preco;
+	}
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+
+}
